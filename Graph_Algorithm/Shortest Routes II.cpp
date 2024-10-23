6b5a3f718966b46c3e82210292c301336af60172
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2024-10-24 01:29:13 +0530

    code

---
+++ Graph_Algorithm/Shortest Routes II.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#define ll long long
+void helper(){
+}
+vector<ll>input(int n){
+  
+    vector<ll>vec;
+    for(int i=0;i<n;i++){
+      ll ele;
+      cin>>ele;
+      vec.emplace_back(ele);
+    }
+    return vec;
+}
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t){
+
+ t--;}
+
+}
